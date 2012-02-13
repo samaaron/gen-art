63a85fad4191bcf84c7979e36a80c48513de316d
Sam Aaron <samaaron@gmail.com>
2012-02-13 23:30:39 +0000

    Make fluffy cloud animation even more faithful to the original

--- src/gen_art/animated_fluffy_clouds.clj
+++ src/gen_art/animated_fluffy_clouds.clj
@@ -62,19 +62,25 @@
     (ellipse 0 0 edge-size (/ edge-size 2))
     (pop-matrix)))
 
-(defn draw-all-points [x-shift y-shift]
-  (dorun
-   (for [x (range-incl 0 (width) 5)
-         y (range-incl 0 (height) 5)]
-     (let [x-noise (mul-add x 0.01 x-shift)
-           y-noise (mul-add y 0.01 y-shift)]
-       (draw-point x y (noise x-noise y-noise))))))
+(defn draw-all-points [x-start y-start]
+  (let [step-size 5
+        x-idxs    (range-incl 0 (/ (width) step-size))
+        y-idxs    (range-incl 0 (/ (height) step-size))]
+    (dorun
+     (for [x-idx x-idxs
+           y-idx y-idxs]
+       (let [x (* step-size x-idx)
+             y (* step-size y-idx)
+             x-noise (mul-add x-idx 0.1 x-start)
+             y-noise (mul-add y-idx 0.1 y-start)]
+         (draw-point x y (noise x-noise y-noise)))))))
+
 
 (defn draw []
   (background 0)
-  (let [x-shift ((state :x-shifts))
-        y-shift ((state :y-shifts))]
-    (draw-all-points x-shift y-shift)))
+  (let [starts-str        (state :starts-str)
+        [x-start y-start] (starts-str)]
+    (draw-all-points x-start y-start)))
 
 (defn setup []
   (size 300 300)
@@ -82,16 +88,15 @@
   (background 0)
   (frame-rate 24)
 
-  (let [x-shifts     (steps (random 10) 0.01)
-        y-shifts     (steps (random 10) 0.01)
-        x-shifts-str (seq->stream x-shifts)
-        y-shifts-str (seq->stream y-shifts)]
+  (let [x-starts (steps (random 10) 0.01)
+        y-starts (steps (random 10) 0.01)
+        starts   (map list x-starts y-starts)
+        starts-str (seq->stream starts)]
 
-    (set-state! :x-shifts x-shifts-str
-                :y-shifts y-shifts-str)))
+    (set-state! :starts-str starts-str)))
 
 (defapplet example
-  :title "Animtied Fluffy Clouds"
+  :title "Animated Fluffy Clouds"
   :setup setup
   :draw draw
   :size [300 300])
