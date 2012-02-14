a67dd66bc2142adc37ad90975f556502919f7601
Sam Aaron <samaaron@gmail.com>
2012-02-14 17:17:23 +0000

    fix spacing bug

--- src/gen_art/sine_wave.clj
+++ src/gen_art/sine_wave.clj
@@ -44,7 +44,7 @@
   (let [xs        (range-incl 20 480 1)
         rads      (map radians (range))
         ys        (map sin rads)
-        scaled-ys (mul-add ys40 50)
+        scaled-ys (mul-add ys 40 50)
         line-args (line-join-points xs scaled-ys)]
     (dorun (map #(apply line %) line-args))))
 
