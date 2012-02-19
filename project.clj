4018c628157e0421d4ada4f18525d2c83114a12e
Sam Aaron <samaaron@gmail.com>
2012-02-19 23:02:48 +0000

    use latest procesing snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject proc "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.automata/rosado.processing "1.1.0"]])
+                 [processing-core/processing.core "1.0.0-SNAPSHOT"]])
